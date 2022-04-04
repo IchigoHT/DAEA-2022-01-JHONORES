50f817c6f5d3d8655cc8cd6cb2a08b62322a53f8
IchigoHT <jose_honores12@hotmail.com>
2022-04-03 23:26:10 -0500

    Lab03 Finalizado

--- Lab03/Properties/AssemblyInfo.cs
+++ Lab03/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Lab02-01")]
+[assembly: AssemblyTitle("Lab03")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab02-01")]
+[assembly: AssemblyProduct("Lab03")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("1a0b44db-87ac-49db-b4f7-3e81a255054c")]
+[assembly: Guid("2342d6db-b6bf-4057-acd1-6e66e81ef100")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
