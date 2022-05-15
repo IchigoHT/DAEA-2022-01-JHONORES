cd378bfaf08e000f01dafbacafe1aa6416c3a666
IchigoHT <jose_honores12@hotmail.com>
2022-05-15 01:19:56 -0500

    Laboratorio 08 Finalizado

---
+++ Laboratorio09/Person.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio09
+{
+    public partial class Person : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+   
+    }
+}
